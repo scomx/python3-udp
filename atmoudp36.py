8c48ee9aee74817a536714d8d12d8f3e69bc0765
atmocom <42433837+atmocom@users.noreply.github.com>
2019-05-09 12:48:05 +0200

    DB table update, precision
    -Added ABS pressure to DB table
    -Changed some column names to correctly reflect values 
    -Fixed weekly/annual rain records storage bug
    -Increased precision in inHg to hPa conversion
    -Values are now rounded to 2 decimals before storing in DB

--- atmoudp36.py
+++ atmoudp36.py
@@ -4,7 +4,7 @@
 ATMOCOM UDP listener captures any data on UDP broadcast 
 and will try to parse and store it to SQLite databases.
 Logic is virtually identical to acpwu.php companion script 
-but over UDP protocol instead of HTTP.
+with communication over UDP protocol instead of HTTP.
 
 License GNU-GPL V3, see repo for details.
 
@@ -29,8 +29,8 @@ class WXParser:
 		global wxdata
 
 		#Initialize dictionary with all supported parameters to avoid potential key errors in SQL exec later
-		wxdata = { 'stationID':'unknownID', 'baro':0, 'temp':0, 'dewpoint':0, 'intemp':0, 'wctemp':0, 'rhum':0, 'inrhum':0, 'wdir':0, 'wgustdir':0, 'wspeed':0,
-			'wgustspeed':0, 'precip':0, 'precip_day':0, 'precip_month':0, 'UV':0, 'solar':0, 'soiltemp':0, 'soilmoisture':0, 'leafwetness':0, 
+		wxdata = { 'stationID':'unknownID', 'baro':0, 'absbaro':0, 'temp':0, 'dewpoint':0, 'intemp':0, 'wctemp':0, 'rhum':0, 'inrhum':0, 'wdir':0, 'wgustdir':0, 'wspeed':0,
+			'wgustspeed':0, 'precip':0, 'precip_day':0, 'precip_week':0, 'precip_month':0, 'precip_year':0, 'UV':0, 'solar':0, 'soiltemp':0, 'soilmoisture':0, 'leafwetness':0, 
 			'weather':0, 'clouds':0, 'visibility':0, 'firmware_rev':'0x000000' }
 
 	def store_data(self, qdic):
@@ -49,35 +49,40 @@ class WXParser:
 				wxdata.update({ 'stationID': 'unknownID' })
 
 			#Get air pressure
-			if 'absbaromin' in qdic: wxdata.update({ 'baro': uc.inHg_hPa(qdic['absbaromin']) })
+			if 'baromin' in qdic: wxdata.update({ 'baro': uc.inHg_hPa(qdic['baromin']) })
+			if 'absbaromin' in qdic: wxdata.update({ 'absbaro': uc.inHg_hPa(qdic['absbaromin']) })
 			
 			#Get/convert temperatures
-			if 'tempf' in qdic: wxdata.update({ 'temp': uc.c_f(qdic['tempf']) })
-			if 'dewptf' in qdic: wxdata.update({ 'dewpoint': uc.c_f(qdic['dewptf']) })
-			if 'indoortempf' in qdic: wxdata.update({ 'intemp': uc.c_f(qdic['indoortempf']) })
-			if 'windchillf' in qdic: wxdata.update({ 'wctemp': uc.c_f(qdic['windchillf']) })
+			if 'tempf' in qdic: wxdata.update({ 'temp': uc.f_c(qdic['tempf']) })
+			if 'dewptf' in qdic: wxdata.update({ 'dewpoint': uc.f_c(qdic['dewptf']) })
+			if 'indoortempf' in qdic: wxdata.update({ 'intemp': uc.f_c(qdic['indoortempf']) })
+			if 'windchillf' in qdic: wxdata.update({ 'wctemp': uc.f_c(qdic['windchillf']) })
 
 			#Get/convert humidity
 			if 'humidity' in qdic: wxdata.update({ 'rhum': float(qdic['humidity']) })
 			if 'indoorhumidity' in qdic: wxdata.update({ 'inrhum': float(qdic['indoorhumidity']) })
 
-			#Get/convert wind and windspeed
-			if 'winddir' in qdic: wxdata.update({ 'wdir': float(qdic['winddir']) })
+			#Get/convert wind and windspeed -- set wind gust dir to prevailing wind dir in case the first is not reported by PWS
+			if 'winddir' in qdic: 
+				wxdata.update({ 'wdir': float(qdic['winddir']) })
+				wxdata.update({ 'wgustdir': float(qdic['winddir']) }) 
 			if 'windgustdir' in qdic: wxdata.update({ 'wgustdir': float(qdic['windgustdir']) })
 			if 'windspeedmph' in qdic: wxdata.update({ 'wspeed': uc.mph_kt(qdic['windspeedmph']) })
 			if 'windgustmph' in qdic: wxdata.update({ 'wgustspeed': uc.mph_kt(qdic['windgustmph']) })
 
 			#Get/convert precipitation
 			if 'rainin' in qdic: wxdata.update({ 'precip': uc.in_mm(qdic['rainin']) })
+			if 'weeklyrainin' in qdic: wxdata.update({ 'precip_week': uc.in_mm(qdic['weeklyrainin']) })
 			if 'dailyrainin' in qdic: wxdata.update({ 'precip_day': uc.in_mm(qdic['dailyrainin']) })
 			if 'monthlyrainin' in qdic: wxdata.update({ 'precip_month': uc.in_mm(qdic['monthlyrainin']) })
+			if 'yearlyrainin' in qdic: wxdata.update({ 'precip_year': uc.in_mm(qdic['yearlyrainin']) })
 
 			#Get UV and solar radiation
 			if 'UV' in qdic: wxdata.update({ 'UV': float(qdic['UV']) })
 			if 'solarradiation' in qdic: wxdata.update({ 'solar': float(qdic['solarradiation']) })
 
 			#Other data
-			if 'soiltempf' in qdic: wxdata.update({ 'soiltemp': uc.c_f(qdic['soiltempf']) })
+			if 'soiltempf' in qdic: wxdata.update({ 'soiltemp': uc.f_c(qdic['soiltempf']) })
 			if 'soilmoisture' in qdic: wxdata.update({ 'soilmoisture': float(qdic['soilmoisture']) })
 			if 'leafwetness' in qdic: wxdata.update({ 'leafwetness': float(qdic['leafwetness']) })
 			if 'weather' in qdic: wxdata.update({ 'weather': float(qdic['weather']) })
@@ -105,7 +110,7 @@ class DBStorage:
 
 		date_str = now.strftime('%Y-%m-%d')
 		time_str = now.strftime('%H:%M:%S')
-		zOffset = (now-datetime.utcnow()).total_seconds()/60.0
+		zOffset = round((now-datetime.utcnow()).total_seconds()/60.0, 1)
 
 		#Check if DB already exists and if not create it		
 		self.db_create(dbname)
@@ -119,7 +124,7 @@ class DBStorage:
 					wxdict['precip'], wxdict['precip_day'], wxdict['UV'], wxdict['solar'],
 					wxdict['intemp'], wxdict['inrhum'], wxdict['soiltemp'], wxdict['soilmoisture'],
 					wxdict['leafwetness'], wxdict['weather'], wxdict['clouds'], wxdict['visibility'],
-					0, 0, 0, 0, wxdict['firmware_rev'] ) )
+					wxdict['precip_week'], wxdict['precip_month'], wxdict['precip_year'], wxdict['absbaro'], wxdict['firmware_rev'] ) )
 
 			dbconn.commit()
 		except sqlite3.Error as e:
@@ -147,9 +152,9 @@ class DBStorage:
 				c.execute( "CREATE TABLE IF NOT EXISTS wxdata (ID integer primary key, STATIONID text, DATE text, TIME text, UTC real, \
 				TEMP real, DEWPT real, RHUM real, BARO real, WINDDIR real, WINDVEL real, WGUSTDIR real, WGUSTVEL real, PRECIP real, PRECIPDAY real, \
 				UVIDX real, SOLAR real, INTEMP real, INRHUM real, SOILTEMP real, SOILMOIST real, LEAFWET real, WEATHER text, CLOUDS text, VISNM real, \
-				RES1 real, RES2 real, RES3 real, RES4 real, RES5 text);")
+				PRECIPWEEK real, PRECIPMON real, PRECIPYEAR real, ABSBARO real, FIRMWARE_REV text);")
 				
-				return conn
+				#return conn
 			else:
 				print('Failed to create database or table')
 
@@ -162,25 +167,25 @@ class DBStorage:
 
 
 class UnitConverter:
-	def c_f(self, v):
+	def f_c(self, v):
 		temp=float(v)
 		if not USE_METRIC: return temp
-		return (temp - 32) * 0.5556
+		return round((temp - 32) * 0.555556, 2)
 
 	def in_mm(self, v):
 		p_in = float(v)
 		if not USE_METRIC: return p_in
-		return p_in * 25.4
+		return round(p_in * 25.4, 2)
 
 	def mph_kt(self, v):
 		ws_mph = float(v)
 		if not USE_METRIC: return ws_mph
-		return ws_mph * 0.869
+		return round(ws_mph * 0.868976, 2)
 	
 	def inHg_hPa(self, v):
 		inHg = float(v)
 		if not USE_METRIC: return inHg
-		return inHg * 33.86
+		return round(inHg * 33.8638816, 2)
 
 
 def main():
@@ -204,7 +209,7 @@ def main():
 		USE_METRIC = False
 
 
-	print("* ATMOCOM UDP Weather Data Logger v0.3b *")
+	print("* ATMOCOM UDP Weather Data Logger v0.4b *")
 	print("-Passkey            : " + PASSKEY)
 	print("-Database directory : " + DATAFOLDER)
 	print("-Using metric system: " + str(USE_METRIC))
